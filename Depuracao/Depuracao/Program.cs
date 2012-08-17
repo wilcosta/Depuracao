ccd1e8e64a1551cce398690e8c7c7d68a52bad88
Wiliam <wiliam_costa@yahoo.com.br>
2012-08-16 22:02:29 -0300

    Adicionei... alguma coisa

--- Depuracao/Depuracao/Program.cs
+++ Depuracao/Depuracao/Program.cs
@@ -34,6 +34,19 @@ namespace Depuracao
           
             else
             Console.WriteLine("O quociente entre {0} e {1} é {2}", numero, denominador, Dividir(numero, denominador));
+
+            int[] vetor = new int[3];
+            vetor[0] = int.Parse(Console.ReadLine());
+            vetor[1] = int.Parse(Console.ReadLine());
+            vetor[2] = int.Parse(Console.ReadLine());
+
+            for (int c = 0; c < vetor.Length; c++)
+            
+                Console.Write("{0}", vetor[c]);
+
+            string nome = null;
+            Console.WriteLine(nome);
+
             Console.Read();
         }
     }
